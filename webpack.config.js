51ff14c5b9ffd9f2caf5bc20c7a6cfe0a70dd1c1
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-12-13 22:25:11 -0600

    se integrro babel en la configuracion de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const path = require("path");
 
-module.exportd = {
+module.exports = {
     entry: "./src/index.js",
     output: {
-        pth: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "dist"),
         filename: "main.js",
     },
-    resolve:{
-        extensions:[".js"]
+    resolve: {
+        extensions: [".js"]
+    },
+    module: {
+        rules: [
+            {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        }
+        ]
     }
 
 }
